8868d4b8e8d15366134e894aa2cc73a050bb718f
AlejandraPacheco <alejandra.pacheco1812@gmail.com>
2023-05-10 23:44:11 -0400

    Ahi esta chicos, pa que loediten

--- cliente/src/router/index.js
+++ cliente/src/router/index.js
@@ -2,31 +2,31 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
   const routes = [
-        {
-          path: '/',
-          name: 'pruebaBackend',
-          component: () => import('../components/pruebaAPI.vue')
-        },
-  //   {
-  //     path: '/',
-  //     name: 'home',
-  //     component: () => import('../components/homeComponent.vue')
-  //   },
-  // {
-  //   path: '/cronograma',
-  //   name: 'cronograma',
-  //   component: () => import('../components/cronogramaComponent.vue')
-  // },
-  // {
-  //   path: '/contacto',
-  //   name: 'contacto',
-  //   component: () => import('../informacionComponent.vue')
-  // },
-  // {
-  //   path: '/login',
-  //   name: 'login',
-  //   component: () => import('../components/loginComponent.vue')
-  // },
+        // {
+        //   path: '/',
+        //   name: 'pruebaBackend',
+        //   component: () => import('../components/pruebaAPI.vue')
+        // },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../components/homeComponent.vue')
+    },
+  {
+    path: '/cronograma',
+    name: 'cronograma',
+    component: () => import('../components/cronogramaComponent.vue')
+  },
+  {
+    path: '/contacto',
+    name: 'contacto',
+    component: () => import('../informacionComponent.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../components/loginComponent.vue')
+  },
 ]
 const router = new VueRouter({
   mode: 'history',
--- servidor/routes.js
+++ servidor/routes.js
@@ -8,4 +8,15 @@ router.get('/api/mensaje', (req, res) => {
     res.send(mensaje);
   });
 
+  router.get('/api/data', (req, res) => {
+    const query = 'SELECT * FROM empresa';
+    db.query(query, (error, results) => {
+      if (error) {
+        res.status(500).send({ error });
+      } else {
+        res.send(results);
+      }
+    });
+  });
+
 module.exports = router;
