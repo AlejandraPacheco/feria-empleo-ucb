9ca5ecf3a9bd0b7abd97259b20ddecd7346290b3
valdiux06 <juan.vada@ucb.edu.bo>
2023-05-11 08:26:57 -0400

    contactos

--- cliente/src/informacionComponent.vue
+++ cliente/src/informacionComponent.vue
@@ -1,16 +1,10 @@
 <template>
+  
   <div>
-    <center>
-    <h1>Contacto</h1>
-    <p>Para contactarnos, por favor utilice la siguiente información:</p>
-    <div>
-      <h2>{{ empresa.nombre }}</h2>
-      <p>Dirección: {{ empresa.direccion }}</p>
-      <p>Teléfono: {{ empresa.telefono }}</p>
-      <p>Correo electrónico: {{ empresa.email }}</p>
-    </div>
-    <p>O complete el siguiente formulario:</p>
-    <form @submit.prevent="submitForm">
+    <div class="column">
+      <h1 class="letra1">¡Contáctanos y caminemos al éxito!</h1>
+      <h1>¿Estás listo para brillar <br> y demostrar de lo que estas hecho?</h1>
+      <form @submit.prevent="submitForm">
       <div>
         <label for="nombre">Nombre:</label>
         <input type="text" id="nombre" v-model="nombre" required>
@@ -25,10 +19,104 @@
       </div>
       <button type="submit">Enviar</button>
     </form>
-  </center>
   </div>
+  <div class="column">
+    <img src="https://lpz.ucb.edu.bo/wp-content/uploads/2021/12/Colores-Vertical-3.png" width="700px" height="700px">
+   
+  </div>
+  <div class="clearfix"></div>
+    
+    
+  <footer class="footer">
+    <p>Derechos de autor &copy; 2023 - Feria del Empleo UCB</p>
+    <p>Para contactarnos, por favor utilice la siguiente información:</p>
+    <div>
+      <h2>{{ empresa.nombre }}</h2>
+      <p>Dirección: {{ empresa.direccion }}</p>
+      <p>Teléfono: {{ empresa.telefono }}</p>
+      <p>Correo electrónico: {{ empresa.email }}</p>
+    </div>
+  </footer>
+  </div>
+  
 </template>
 
+<style>
+   .column {
+      float: left;
+      width: 50%;
+      padding: 10px;
+      box-sizing: border-box;
+    }
+    .footer {
+      background-color: teal;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+    .clearfix::after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+.letra1{
+  font-size: 100px;
+}
+body {
+  font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      margin: 0;
+      padding: 0;
+  
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+form {
+  margin: 20px auto;
+  max-width: 400px;
+  padding: 20px;
+  background: #f4f4f4;
+  border-radius: 5px;
+}
+
+input[type="text"],
+input[type="email"],
+textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+button {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  background: #333;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+#contact-list {
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+.contact {
+  padding: 10px;
+  background: #f4f4f4;
+  margin-bottom: 10px;
+  border-radius: 5px;
+}
+</style>
 <script>
 export default {
   data() {
