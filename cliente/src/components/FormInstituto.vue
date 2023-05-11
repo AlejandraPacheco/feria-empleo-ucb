838687f3173e9c2bfe523c03fd4110c8cf450111
VaniaPatzi <vania.patzi@ucb.edu.bo>
2023-05-11 06:55:38 -0400

    Agregado carousel

---
+++ cliente/src/components/FormInstituto.vue
@@ -0,0 +1,40 @@
+<template>
+    <form>
+      <label for="nombre">Nombre de la empresa</label>
+      <input type="text" id="nombre" v-model="nombre">
+  
+      <label for="ubicacion">Ubicación</label>
+      <input type="text" id="ubicacion" v-model="ubicacion">
+  
+      <label for="descripcion">Descripción</label>
+      <textarea id="descripcion" v-model="descripcion"></textarea>
+  
+      <label for="logo">Logo</label>
+      <input type="file" id="logo" @change="onFileSelected">
+  
+      <button type="submit" @click.prevent="submitForm">Guardar</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        nombre: '',
+        ubicacion: '',
+        descripcion: '',
+        logo: null
+      }
+    },
+  
+    methods: {
+      onFileSelected(event) {
+        this.logo = event.target.files[0]
+      },
+  
+      submitForm() {
+        // Aquí puedes agregar la lógica para enviar los datos del formulario al servidor
+      }
+    }
+  }
+  </script>
